7f5fb9071ef63bbf77a3ddbc2934344ece416b69
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2019-03-26 09:12:43 -0500

    ssl stuff update

--- ssl/eugridpma-robot-cert-req.sh
+++ ssl/eugridpma-robot-cert-req.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 key=$1
+fqdn=$2
 
 openssl req -config eugridpma-robot-openssl.cnf -new \
     -key $key \
-    -subj "/CN=admin@icecube.wisc.edu/CN=Robot - Rucio FTS"
+    -subj "/C=US/ST=Wisconsin/O=University of Wisconsin-Madison/OU=OCIS/CN=$fqdn/emailAddress=admin@icecube.wisc.edu/CN=Robot - Rucio FTS"
--- ssl/make_cert_request.sh
+++ ssl/make_cert_request.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 key=$1
-cn=$2
+fqdn=$2
 
-openssl req -new -key $key \
-    -subj "/CN=$cn" # I think DoIT provides all other fields
-    #-subj "/C=US/ST=Wisconsin/L=Madison/O=University of Wisconsin at Madison/OU=IceCube/CN=$cn"
+openssl req -new \
+    -key $key \
+    -subj "/C=US/ST=Wisconsin/L=Madison/O=University of Wisconsin-Madison/OU=OCIS/CN=$fqdn"
